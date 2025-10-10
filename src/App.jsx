561564e4535c32138cb8e08b135ff81e5c72a9b7
Max <mc.cui@mail.utoronto.ca>
2025-10-10 17:02:23 -0400

    new project added

--- src/App.jsx
+++ src/App.jsx
@@ -30,50 +30,67 @@ function App() {
       description: "Hybrid Quantum-Classical Graph Generative Adversarial Network for Molecular Generation,",
       technologies: ["PennyLane", "PyTorch"],
       image: "funkymols.png",
-      githubUrl: "https://github.com/Raiden-Makoto/QWGraphGAN-GP"
+      githubUrl: "https://github.com/Raiden-Makoto/QWGraphGAN-GP",
+      deployed: false,
+      isResearch: true
     },
     {
       title: "Wordle Solver",
       description: "An entropy-based program that solves Wordle in an average of 3.8 guesses, with a 100\% success rate on 1534 past Wordles.",
       technologies: ["Python", "Flask", "HTML/CSS"],
       image: "wordlesolver.png",
-      githubUrl: "https://42cummer-entropywordlesolver.hf.space/"
+      githubUrl: "https://42cummer-entropywordlesolver.hf.space/",
+      deployed: true
     },
     {
       title: "WheresMyBus",
       description: "Web application providing live Toronto Transit Commission (TTC) bus and streetcar departure times and vehicle tracking to help you plan your commute on the fly.",
       technologies: ["JavaScript", "React", "HTML/CSS", "Flask", "Docker"],
       image: "900express.png",
-      githubUrl: "https://wheresmybus.vercel.app/"
+      githubUrl: "https://wheresmybus.vercel.app/",
+      deployed: true
     },
     {
       title: "Genshinfy V2",
       description: "AI-powered style transfer tools for generating an anime-style avatar of your face from Genshin Impact character references. You can also just generte avatars based on which ever character you want.",
       technologies: ["Transformers", "Gradio", "PyTorch", "Diffusers", "DinoVision"],
       image: "genshinfy.png",
-      githubUrl: "https://42cummer-genshinfyv2.hf.space/"
+      githubUrl: "https://42cummer-genshinfyv2.hf.space/",
+      deployed: true
     },
     {
       title: "Tetris",
       description: "Implemented Tetris in MIPS Assembly. Gameplay (tetromino movement, rotation, line clearing) optimized for performance on a bitmap display. Features custom SFX and power-ups not found in the original version of the game.",
       technologies: ["MIPS Assembly"],
       image: "tetris.png",
-      githubUrl: "https://github.com/Raiden-Makoto/Tetris"
+      githubUrl: "https://github.com/Raiden-Makoto/Tetris",
+      deployed: false
+    },
+    {
+      title: "Multi-Conditional Graph Variational Autoencoder",
+      description: "This project implements a Conditional Graph Variational Autoencoder (CGVAE) for generating molecular structures with desired properties. The specific application is designing molecules with controlled blood-brain barrier (BBB) permeability - a critical factor in developing drugs that target the central nervous system",
+      technologies: ["PyTorch", "PyTorch Geometric", "Python"],
+      image: "funkymols.png",
+      githubUrl: "https://github.com/Raiden-Makoto/MCGVAE",
+      deployed: false
     },
     {
       title: "SoundofMusic",
       description: "Pipeline to generate lyrics and matching melody and harmony for pop songs in the style of Taylor Swift.",
       technologies: ["Transformers", "PyTorch", "PyQT", "Music21"],
       image: "soundofmusic.png",
-      githubUrl: "https://github.com/Raiden-Makoto/SoundOfMusic"
+      githubUrl: "https://github.com/Raiden-Makoto/SoundOfMusic",
+      deployed: false
     },
     {
       title: "67",
       description: "A Chrome Extension that overlays subway surfers gameplay similar to TikTok/IG split screen. This project aims to increase ADHD attention in Gen-Z. For more information, see the GitHub repo.",
       technologies: ["HTML/CSS", "JavaScript"],
       image: "67.png",
-      githubUrl: "https://github.com/Raiden-Makoto/67GoonCornerSybau"
-    }
+      githubUrl: "https://github.com/Raiden-Makoto/67GoonCornerSybau",
+      deployed: false,
+      isHackathon: true
+    },
   ]
 
   // Auto-cycle through projects every 30 seconds
@@ -187,34 +204,19 @@ function App() {
             
             <div className={`carousel-content fade-transition ${isTransitioning ? 'fade-out' : 'fade-in'}`}>
               <div className="project-card">
-                {currentProject === 0 && (
+                {projects[currentProject].isResearch && (
                   <div className="hackathon-badge">
                     <span>RESEARCH PROJECT</span>
                   </div>
                 )}
-                {currentProject === 1 && (
+                {projects[currentProject].deployed && (
                   <div className="hackathon-badge">
                     <span>DEPLOYED</span>
                   </div>
                 )}
-                {currentProject === 2 && (
+                {projects[currentProject].isHackathon && (
                   <div className="hackathon-badge">
-                    <span>DEPLOYED</span>
-                  </div>
-                )}
-                {currentProject === 3 && (
-                  <div className="hackathon-badge">
-                    <span>DEPLOYED</span>
-                  </div>
-                )}
-                {currentProject === 5 && (
-                  <div className="hackathon-badge in-progress">
-                    <span>DEPLOYED</span>
-                  </div>
-                )}
-                {currentProject === 6 && (
-                  <div className="hackathon-badge">
-                    <span>Hackathon Project</span>
+                    <span>HACKATHON PROJECT</span>
                   </div>
                 )}
                 <a href={projects[currentProject].githubUrl} target="_blank" rel="noopener noreferrer">
